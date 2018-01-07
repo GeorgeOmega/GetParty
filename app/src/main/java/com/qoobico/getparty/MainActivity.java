3a51bb17b15c270195611156dc34658a7ce74dc9
GeorgeOmega <georgeomegamail93@gmail.com>
2018-01-08 00:36:45 +0700

    Edited NavigationView,added Message_item and Navigation_header in NavigationView

--- app/src/main/java/com/qoobico/getparty/MainActivity.java
+++ app/src/main/java/com/qoobico/getparty/MainActivity.java
@@ -1,7 +1,7 @@
 package com.qoobico.getparty;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
@@ -12,15 +12,28 @@ import static com.qoobico.getparty.R.menu.menu;
  */
 
 public class MainActivity extends AppCompatActivity {
+    private static final int LAYOUT=R.layout.activity_main;
+
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
         initToolbar();
+        initNavigationView();
     }
+
+    private void initNavigationView()
+    {
+        drawerLayout=(DrawerLayout) findViewById(R.id.drawer_layout);
+
+
+    };
     private void initToolbar()
     {
         toolbar=(Toolbar) findViewById(R.id.toolbar);
