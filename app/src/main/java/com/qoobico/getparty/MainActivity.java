aec54d31142ad43b8ceb133c76bd0273511c53b5
GeorgeOmega <georgeomegamail93@gmail.com>
2018-01-07 16:15:03 +0700

    Created MainActivity class and layout

---
+++ app/src/main/java/com/qoobico/getparty/MainActivity.java
@@ -0,0 +1,17 @@
+package com.qoobico.getparty;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by George Omega on 07.01.2018.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
