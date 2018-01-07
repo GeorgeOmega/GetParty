cfe1e116993fdcb7dc0fb66acd93b574baf06a68
GeorgeOmega <georgeomegamail93@gmail.com>
2018-01-07 21:02:17 +0700

    Added Toolbar and Search Icon

--- app/src/main/java/com/qoobico/getparty/MainActivity.java
+++ app/src/main/java/com/qoobico/getparty/MainActivity.java
@@ -1,17 +1,37 @@
 package com.qoobico.getparty;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
+import static com.qoobico.getparty.R.menu.menu;
 
 /**
  * Created by George Omega on 07.01.2018.
  */
 
 public class MainActivity extends Activity {
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
+    }
+    private void initToolbar()
+    {
+        toolbar=(Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(menu);
     }
 }
