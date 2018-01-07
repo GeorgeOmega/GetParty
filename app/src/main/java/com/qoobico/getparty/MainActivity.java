f9dc7fcfa6761fcd3752dda63ae3653cc299f144
GeorgeOmega <georgeomegamail93@gmail.com>
2018-01-07 22:43:06 +0700

    Edited NavigationView,Activity_main and toolbar

--- app/src/main/java/com/qoobico/getparty/MainActivity.java
+++ app/src/main/java/com/qoobico/getparty/MainActivity.java
@@ -2,6 +2,7 @@ package com.qoobico.getparty;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
 import static com.qoobico.getparty.R.menu.menu;
@@ -10,11 +11,12 @@ import static com.qoobico.getparty.R.menu.menu;
  * Created by George Omega on 07.01.2018.
  */
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initToolbar();
